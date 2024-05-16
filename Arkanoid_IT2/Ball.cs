bafd9b81f36297c301e87aab854a8f9e46fa54d5
ArticFox93 <pavelsmida16@seznam.cz>
2024-05-16 15:16:22 +0200

    Přidejte soubory projektu.

---
+++ Arkanoid_IT2/Ball.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Arkanoid_IT2
+{
+    public class Ball : GameObject
+    {
+        // co by mel mit ball za vlastnosti:
+        // velikost, rychlost pohybu, pohyb (x, y), barvu
+
+        public int Size { get; set; }
+
+        public Vector VectorMove { get; set; }
+        //public SolidColorBrush color;
+
+        // double speed, double xpos, double ypos, SolidColorBrush brush
+        // Speed = speed;
+        // XPos = xpos;
+        // YPos = ypos;
+        // color = brush;
+        public Ball(int size)
+        {
+            Size = size;
+
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            //Ellipse ball = new Ellipse
+            //{
+            //    Width = Size,
+            //    Height = Size,
+            //    Fill = Brushes.White,
+            //};
+            //Canvas.SetLeft(ball, Location.X + Size);
+            //Canvas.SetTop(ball, Location.Y + Size);
+
+            //canvas.Children.Add(ball);
+
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = Size;
+            ellipse.Height = Size;
+            ellipse.Fill = Brushes.White;
+            Canvas.SetBottom(ellipse, Location.Y - Size / 2);
+            Canvas.SetLeft(ellipse, Location.X - Size / 2);
+            canvas.Children.Add(ellipse);
+
+        }
+
+        public void Move()
+        {
+            Location.Offset(VectorMove.X, VectorMove.Y);
+        }
+
+    }
+}
---
+++ Arkanoid_IT2/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Arkanoid_IT2
+{
+    public class Game
+    {
+        private Board board;
+        private Ball ball;
+        public List<GameObject> GameObjects { get; set; }
+        //private Timer timerEngine;
+
+        public Game()
+        {
+             //timerEngine = new Timer();
+             //timerEngine.Interval = 20;
+             //timerEngine.Elapsed += TimerEngine_Elapsed;
+             GameObjects = new List<GameObject>();
+             CreateLevel(1);
+        }
+
+        //private void TimerEngine_Elapsed(object? sender, ElapsedEventArgs e)
+        //{
+        //    ball.Move();
+        //}
+
+        public void CreateLevel(int level)
+        {
+               board = new Board(50);
+               GameObjects.Add(board);
+               ball = new Ball(20);
+               ball.VectorMove = new Vector(1, 1);
+               ball.Location = new Point(200, 200);
+               GameObjects.Add(ball);
+        }
+
+        public void SetBoardLocation(double x)
+        {
+             board.Location = new Point(x, board.Location.Y);
+        }
+
+        public void SetBallLocation(double x, double y)
+        {
+            ball.Location = new Point(ball.Location.X, ball.Location.Y);
+        }
+
+
+
+        public void Draw(Canvas canvas)
+        {
+             foreach (GameObject obj in GameObjects)
+             {
+                  if (obj != null)
+                  {
+                   obj.Draw(canvas);
+                  }
+             }
+        }
+    }
+}
